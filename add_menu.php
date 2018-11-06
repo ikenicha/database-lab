1079705bdecc49a2ff566a56a7e0226c296349e0
Nicha <44809530+ikenicha@users.noreply.github.com>
2018-11-06 21:53:57 +0700

    Add files via upload

---
+++ add_menu.php
@@ -0,0 +1,22 @@
+
+<!DOCTYPE html>
+
+<html>
+<head>
+    <title>Add menu</title>
+</head>
+<body>
+    <form action='' method='post'>
+        ProductName : <input type='text' placeholder='Enter ProductName' name='pname'><br><br>
+        Cal : <input type='text' placeholder='Enter Cal' name='cal'><br><br>
+        Price : <input type='text' placeholder='Enter Price' name='price'><br><br>
+        ProductType: <select  name='pid'>
+  <option value="1">Main Course</option>
+  <option value="2">Desserts</option>
+  <option value="3">Drink</option>
+</select><br><br>
+imgurl : <input type='text' placeholder='Enter imgurl'  name='url'><br><br>
+<input type='submit'> <a href='edit_menu.php'>Back</a>
+</form>
+</body>
+</html>
